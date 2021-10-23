6f0b8f8238ca6d17013e1877b5a74cb3e53ab442
Noah Chong <atachongnoah@gmail.com>
2021-10-23 10:25:30 -0400

    login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import {auth, database} from '../utils/firebase'
 import { Redirect, useHistory } from 'react-router'
 import { TextField, Button } from '@mui/material'
+import NavBar from '../components/NavBar'
 
 export default function Login() {
     const history = useHistory()
@@ -65,11 +66,25 @@ export default function Login() {
 
     return (
         <div>
-            <h1>THIS IS THE LOGIN</h1>
-            <TextField value={email} label='Email' onChange={handleEmailInputChange}/>
-            <TextField value={password} type='password' label='Password' onChange={handlePasswordInputChange}/>
-            <Button onClick={handleSignUp}>Register</Button>
-            <Button onClick={handleLogIn}>Login</Button>
+            <NavBar />
+            <div className="grid grid-cols-2 gap-10 h-10/12 m-40">
+              <div>
+                <img
+                  src="https://images.unsplash.com/photo-1485217988980-11786ced9454?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
+                  className="h-full object-cover"
+                />
+              </div>
+            
+              <div>
+                <div className="h-full justify-center items-center align-middle grid grid-cols-1 gap-0">
+                <h2 className="text-2xl font-medium tracking-tight text-center text-gray-900">Sign In</h2>
+                  <TextField value={email} label='Email' onChange={handleEmailInputChange}/>
+                  <TextField value={password} type='password' label='Password' onChange={handlePasswordInputChange}/>
+                  <Button onClick={handleSignUp}>Register</Button>
+                  <Button onClick={handleLogIn}>Login</Button>
+                </div>
+              </div>
+            </div>
         </div>
     )
 }
