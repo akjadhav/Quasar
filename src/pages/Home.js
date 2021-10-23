609094f9c6eb99cc0163dac5342140bea45c3315
Noah Chong <atachongnoah@gmail.com>
2021-10-23 00:33:00 -0400

    -Added header component -mock product library

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,70 @@
 import React from 'react'
+import Header from '../components/Header'
 
-export default function Home() {
+const products = [
+    {
+      id: 1,
+      name: 'Basic Tee',
+      href: '#',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: '$35',
+      color: 'Black',
+    },
+    {
+        id: 2,
+        name: 'Chair',
+        href: '#',
+        imageSrc: 'https://m.media-amazon.com/images/I/41tCIsGV8UL.jpg',
+        imageAlt: "A brown chair",
+        price: '$75',
+        color: 'Brown',
+      },
+  ]
+
+const navigation = [
+    { name: 'Solutions', href: '#' },
+    { name: 'Pricing', href: '#' },
+    { name: 'Docs', href: '#' },
+    { name: 'Company', href: '#' },
+  ]
+  
+  export default function Home() {
     return (
-        <div>
-            <h1>THIS IS THE HOMEPAGE</h1>
+    <div>
+      <Header />
+      <div className="bg-white">
+        <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+          <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">Items for Sale</h2>
+  
+          <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+            {products.map((product) => (
+              <div key={product.id} className="group relative">
+                <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-96 lg:aspect-none">
+                  <img
+                    src={product.imageSrc}
+                    alt={product.imageAlt}
+                    className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                  />
+                </div>
+                <div className="mt-4 flex justify-between">
+                  <div>
+                    <h3 className="text-sm text-gray-700">
+                      <a href={product.href}>
+                        <span aria-hidden="true" className="absolute inset-0" />
+                        {product.name}
+                      </a>
+                    </h3>
+                    <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                  </div>
+                  <p className="text-sm font-medium text-gray-900">{product.price}</p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
+      </div>
+    </div>
     )
-}
+  }
+  
