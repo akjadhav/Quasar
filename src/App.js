007fc955d3cbb31cc7bc891abbe38091cf71388a
Ayden Marshall <aydenwilliammarshall@gmail.com>
2021-10-22 23:21:46 -0400

    create-react-app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Home from './pages/Home'
+function App() {
+  return (
+    <div>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            <h1>THIS IS THE HOMEPAGE</h1>
+        </div>
+    )
+}
